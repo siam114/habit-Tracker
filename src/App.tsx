d549035ddfb78461bdac3c9e94345d61646440ad
siam114 <smsiamislam598@gmail.com>
2025-08-28 01:34:21 +0600

    use material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { Provider } from 'react-redux'
 import './App.css'
 import store from './store/store'
+import { Container, Typography } from '@mui/material'
 
 function App() {
 
   return (
   <Provider store={store}>
-      <div>
-      subscribe to roadsidecoder
-    </div>
+    <Container>
+      <Typography component="h1" variant='h2' align='center' marginY={4}>Habit Tracker</Typography>
+    </Container>
   </Provider>
   )
 }
---
+++ src/components/add-habit-form.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddHabitForm: React.FC = () => {
+  return (
+    <div>
+      Add Habit Form
+    </div>
+  )
+}
+
+export default AddHabitForm
