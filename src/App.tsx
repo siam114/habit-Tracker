049e18bf1f6fd75c13aa76b67ab5eee6d6949b5d
siam114 <smsiamislam598@gmail.com>
2025-08-29 02:28:31 +0600

    design form

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import { Provider } from 'react-redux'
-import './App.css'
-import store from './store/store'
-import { Container, Typography } from '@mui/material'
+import { Provider } from "react-redux";
+import "./App.css";
+import store from "./store/store";
+import { Container, Typography } from "@mui/material";
+import AddHabitForm from "./components/add-habit-form";
 
 function App() {
-
   return (
-  <Provider store={store}>
-    <Container>
-      <Typography component="h1" variant='h2' align='center' marginY={4}>Habit Tracker</Typography>
-    </Container>
-  </Provider>
-  )
+    <Provider store={store}>
+      <Container>
+        <Typography component="h1" variant="h2" align="center" marginY={4}>
+          Habit Tracker
+        </Typography>
+        <AddHabitForm/>
+      </Container>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
--- src/components/add-habit-form.tsx
+++ src/components/add-habit-form.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { Box, TextField } from "@mui/material";
+import React, { useState } from "react";
 
 const AddHabitForm: React.FC = () => {
+  const [name, setName] = useState<string>("");
+  const [frequescy, setFrequency] = useState<"Daily" | "Weekly">("Daily");
   return (
-    <div>
-      Add Habit Form
-    </div>
-  )
-}
+    <form>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+        <TextField
+          label="Habit Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter Habit Name"
+          fullWidth
+        />
+      </Box>
+    </form>
+  );
+};
 
-export default AddHabitForm
+export default AddHabitForm;
