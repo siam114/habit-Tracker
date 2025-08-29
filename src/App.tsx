e913b4b39bd46c86d013ce92ba550773d5f4b42f
siam114 <smsiamislam598@gmail.com>
2025-08-30 01:27:41 +0600

    design habit list

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import store from "./store/store";
 import { Container, Typography } from "@mui/material";
 import AddHabitForm from "./components/add-habit-form";
+import HabitList from "./components/habit-list";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           Habit Tracker
         </Typography>
         <AddHabitForm/>
+        <HabitList/>
       </Container>
     </Provider>
   );
--- src/components/habit-list.tsx
+++ src/components/habit-list.tsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import type { RootState } from "../store/store";
+import { Box, Button, Grid, Paper, Typography } from "@mui/material";
 
-const HabitList = () => {
+const HabitList: React.FC = () => {
+  const { habits } = useSelector((state: RootState) => state.habits);
+
+  const today = new Date().toISOString().split("T")[0];
+  
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 4 }}>
+      {habits.map((habit) => {
+        return (
+          <Paper key={habit.id} elevation={2} sx={{ p: 2 }}>
+            <Grid container alignItems="center">
+              <Grid>
+                <Typography variant="h6">{habit.name}</Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  sx={{ textTransform: "capitalize" }}
+                >
+                  {habit.frequency}
+                </Typography>
+              </Grid>
+              <Grid xs={12} sm={6}>
+                <Box
+                  sx={{ display: "flex", justifyContent: "flex-end", gap: 1 }}
+                >
+                  <Button
+                    variant="outlined"
+                    color={
+                      habit.completedDates.includes(today)
+                        ? "success"
+                        : "primary"
+                    }
+                  >
+                    Mark Complete
+                  </Button>
+                </Box>
+              </Grid>
+            </Grid>
+          </Paper>
+        );
+      })}
+    </Box>
+  );
+};
 
-export default HabitList
+export default HabitList;
