b74786373ca68930880ac0f6da633d145bd5038d
siam114 <smsiamislam598@gmail.com>
2025-08-31 00:53:33 +0600

    design habit stats component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import store from "./store/store";
 import { Container, Typography } from "@mui/material";
 import AddHabitForm from "./components/add-habit-form";
 import HabitList from "./components/habit-list";
+import HabitStats from "./components/habit-stats";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         </Typography>
         <AddHabitForm/>
         <HabitList/>
+        <HabitStats/>
       </Container>
     </Provider>
   );
