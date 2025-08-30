36122a216eb6b28928b0d2ab741d82b6e0f31344
siam114 <smsiamislam598@gmail.com>
2025-08-31 00:45:24 +0600

    linear progress bar design

--- src/components/habit-list.tsx
+++ src/components/habit-list.tsx
@@ -1,16 +1,33 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "../store/store";
-import { Box, Button, Grid, Paper, Typography } from "@mui/material";
+import { Box, Button, Grid, LinearProgress, Paper, Typography } from "@mui/material";
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import DeleteIcon from "@mui/icons-material/Delete";
-import { removeHabit } from "../store/habit-slice";
+import { removeHabit, toggleHabit, type Habit } from "../store/habit-slice";
 
 const HabitList: React.FC = () => {
   const { habits } = useSelector((state: RootState) => state.habits);
    const dispatch = useDispatch<AppDispatch>();
 
   const today = new Date().toISOString().split("T")[0];
+
+    const getStreak = (habit: Habit) => {
+    let streak = 0;
+    const currentDate = new Date();
+
+    while (true) {
+      const dateString = currentDate.toISOString().split("T")[0];
+      if (habit.completedDates.includes(dateString)) {
+        streak++;
+        currentDate.setDate(currentDate.getDate() - 1);
+      } else {
+        break;
+      }
+    }
+
+    return streak;
+  };
   
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 4 }}>
@@ -40,6 +57,9 @@ const HabitList: React.FC = () => {
                         ? "success"
                         : "primary"
                     }
+                     onClick={() =>
+                    dispatch(toggleHabit({ id: habit.id, date: today }))
+                  }
                     startIcon={<CheckCircleIcon/>}
                   >
                     {habit.completedDates.includes(today)
@@ -57,6 +77,16 @@ const HabitList: React.FC = () => {
                 </Box>
               </Grid>
             </Grid>
+             <Box sx={{ mt: 2 }}>
+            <Typography variant="body2">
+              Current Streak: {getStreak(habit)} days
+            </Typography>
+            <LinearProgress
+              variant="determinate"
+              value={(getStreak(habit) / 30) * 100}
+              sx={{ mt: 1 }}
+            />
+          </Box>
           </Paper>
         );
       })}
